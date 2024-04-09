5ae178f80859401fcca7562c311304681b36959c
刘伟 <530150725@qq.com>
2024-04-09 12:41:02 +0800

    更新环境判断方式

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,10 +1,12 @@
-let BASE_URL = ''
+let BASE_URL = import.meta.env.VITE_BASE_URL
 
-if (import.meta.env.PROD) {
-  BASE_URL = ''
-} else {
-  BASE_URL = ''
-}
+// console.log('BASE_URL', BASE_URL)
+
+// if (import.meta.env.PROD) {
+//   BASE_URL = ''
+// } else {
+//   BASE_URL = ''
+// }
 
 export { BASE_URL }
 
