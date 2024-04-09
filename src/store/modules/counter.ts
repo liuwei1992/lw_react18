531d7a983ada02ab89cf98e100e2b228d8b08005
刘伟 <530150725@qq.com>
2024-04-09 13:15:33 +0800

    store 增加类型

--- src/store/modules/counter.ts
+++ src/store/modules/counter.ts
@@ -1,12 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+interface IState {
+  count: number
+}
+
+const initialState: IState = {
+  count: 10,
+}
 
 const slice = createSlice({
   name: 'counter',
-  initialState: {
-    count: 10,
-  },
+  initialState,
   reducers: {
-    addCountAction: (store, { payload }) => {
+    addCountAction: (store, { payload }: PayloadAction<number>) => {
       store.count += payload
     },
   },
